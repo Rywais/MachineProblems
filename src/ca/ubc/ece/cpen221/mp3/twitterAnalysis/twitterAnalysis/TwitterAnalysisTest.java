25143215ce53aae66868270726434d6f33eb5d0b
Ryan <rywais@gmail.com>
2015-10-25 22:32:22 -0700

    Updated twitteranalysis test to include a string splitting

--- src/ca/ubc/ece/cpen221/mp3/twitterAnalysis/twitterAnalysis/TwitterAnalysisTest.java
+++ src/ca/ubc/ece/cpen221/mp3/twitterAnalysis/twitterAnalysis/TwitterAnalysisTest.java
@@ -51,7 +51,9 @@ public class TwitterAnalysisTest {
 		amg.addEdge(v[5],v[3]);
 		amg.addEdge(v[9],v[3]);
 		
-		String[] s = {"commonInfluencers", "1", "4", "?"}; 
+		String a = "commonInfluencers 1 4 ?";
+		
+		String[] s = a.split(" ");
 		
 		//Change the string here to the desired output file
 		TwitterAnalysis.printQueryResult(s, "C:/Workspaces/OUTPUT.txt");
