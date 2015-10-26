b0fb03dc5395c55fd083dd390e12381afb1ff374
Ryan <rywais@gmail.com>
2015-10-25 17:01:18 -0700

    IMplemented common*Vertices algorithms and test cases

--- src/ca/ubc/ece/cpen221/mp3/graph/Algorithms.java
+++ src/ca/ubc/ece/cpen221/mp3/graph/Algorithms.java
@@ -2,6 +2,7 @@ package ca.ubc.ece.cpen221.mp3.graph;
 
 import ca.ubc.ece.cpen221.mp3.staff.Graph;
 import ca.ubc.ece.cpen221.mp3.staff.Vertex;
+import java.util.*;
 
 public class Algorithms {
 
@@ -29,5 +30,40 @@ public class Algorithms {
 	}
 	
 	
+	public static List<Vertex> commonUpstreamVertices(Graph graph, Vertex a, Vertex b){
+		List<Vertex> result, aList, bList;
+		result = new ArrayList<Vertex>();
+		aList = graph.getUpstreamNeighbors(a);
+		bList = graph.getUpstreamNeighbors(b);
+		
+		//Add equal vertices to result list
+		for(Vertex i : aList){
+			for(Vertex j : bList){
+				if(i.getLabel().equals(j.getLabel()))
+					result.add(i);
+			}
+		}
+		
+		return result;
+	}
+	
+	public static List<Vertex> commonDownstreamVertices(Graph graph, Vertex a, Vertex b){
+		List<Vertex> result, aList, bList;
+		result = new ArrayList<Vertex>();
+		aList = graph.getDownstreamNeighbors(a);
+		bList = graph.getDownstreamNeighbors(b);
+		
+		//Add equal vertices to result list
+		for(Vertex i : aList){
+			for(Vertex j : bList){
+				if(i.getLabel().equals(j.getLabel())){
+					result.add(i);
+					break;
+				}
+			}
+		}
+		
+		return result;
+	}
 
 }
---
+++ src/ca/ubc/ece/cpen221/mp3/graph/AlgorithmsTest.java
@@ -0,0 +1,119 @@
+package ca.ubc.ece.cpen221.mp3.graph;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import ca.ubc.ece.cpen221.mp3.staff.*;
+
+import java.util.*;
+
+public class AlgorithmsTest {
+
+	/**
+	 * Tests that the commonUpstreamVertices Algorithm works properly
+	 * based on known data
+	 */
+	@Test
+	public void commonUpstreamVerticesTest() {
+		Graph amg = new AdjacencyMatrixGraph();
+		Graph alg = new AdjacencyListGraph();
+		Vertex[] v = new Vertex[10];
+		
+		for(int i = 0; i < 10; i++){
+			v[i] = new Vertex(Integer.toString(i+1));
+			amg.addVertex(v[i]);
+			alg.addVertex(v[i]);
+		}
+		
+		alg.addEdge(v[6], v[0]);
+		alg.addEdge(v[1], v[0]);
+		alg.addEdge(v[3], v[0]);
+		alg.addEdge(v[5], v[0]);
+		
+		amg.addEdge(v[6], v[0]);
+		amg.addEdge(v[1], v[0]);
+		amg.addEdge(v[3], v[0]);
+		amg.addEdge(v[5], v[0]);
+		
+		alg.addEdge(v[1],v[3]);
+		alg.addEdge(v[5],v[3]);
+		alg.addEdge(v[9],v[3]);
+		
+		amg.addEdge(v[1],v[3]);
+		amg.addEdge(v[5],v[3]);
+		amg.addEdge(v[9],v[3]);
+		
+		List<Vertex> amgList = Algorithms.commonUpstreamVertices(amg, v[0],v[3]);
+		List<Vertex> algList = Algorithms.commonUpstreamVertices(amg,v[0],v[3]);
+		
+		assertTrue(amgList.size() == algList.size() && 
+				amgList.size() == 2);
+		
+		for(Vertex i : amgList){
+			assertTrue(i.getLabel().equals(v[1].getLabel()) ||
+					i.getLabel().equals(v[5].getLabel()));
+		}
+		
+		for(Vertex i : algList){
+			assertTrue(i.getLabel().equals(v[1].getLabel()) ||
+					i.getLabel().equals(v[5].getLabel()));
+		}
+	}
+	
+	/**
+	 * Tests that the commonDownstreamVertices algorithm works properly
+	 * based on known data
+	 */
+	@Test
+	public void commonDownstreamVerticesTest() {
+		Graph amg = new AdjacencyMatrixGraph();
+		Graph alg = new AdjacencyListGraph();
+		Vertex[] v = new Vertex[10];
+		
+		for(int i = 0; i < 10; i++){
+			v[i] = new Vertex(Integer.toString(i+1));
+			amg.addVertex(v[i]);
+			alg.addVertex(v[i]);
+		}
+		
+		alg.addEdge(v[0], v[1]);
+		alg.addEdge(v[0], v[2]);
+		alg.addEdge(v[0], v[4]);
+		alg.addEdge(v[0], v[6]);
+		
+		amg.addEdge(v[0], v[1]);
+		amg.addEdge(v[0], v[2]);
+		amg.addEdge(v[0], v[4]);
+		amg.addEdge(v[0], v[6]);
+		
+		alg.addEdge(v[3],v[1]);
+		alg.addEdge(v[3],v[8]);
+		alg.addEdge(v[3],v[4]);
+		alg.addEdge(v[3],v[5]);
+		
+		amg.addEdge(v[3],v[1]);
+		amg.addEdge(v[3],v[8]);
+		amg.addEdge(v[3],v[4]);
+		amg.addEdge(v[3],v[5]);
+		
+		List<Vertex> amgList = Algorithms.commonDownstreamVertices(amg, v[0],v[3]);
+		List<Vertex> algList = Algorithms.commonDownstreamVertices(amg,v[0],v[3]);
+		
+		assertTrue(amgList.size() == algList.size() && 
+				amgList.size() == 2);
+		
+		for(Vertex i : amgList){
+			assertTrue(i.getLabel().equals(v[1].getLabel()) ||
+					i.getLabel().equals(v[4].getLabel()));
+		}
+		
+		for(Vertex i : algList){
+			assertTrue(i.getLabel().equals(v[1].getLabel()) ||
+					i.getLabel().equals(v[4].getLabel()));
+		}
+	}
+	
+	
+
+}
