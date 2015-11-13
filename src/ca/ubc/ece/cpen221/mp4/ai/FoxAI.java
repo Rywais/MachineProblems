eb6153d130f66efd69a601b880a0a7d4f0009128
Aviral Garg <aviral@hotmail.ca>
2015-11-12 17:08:21 -0800

    FoxAI added

--- src/ca/ubc/ece/cpen221/mp4/ai/FoxAI.java
+++ src/ca/ubc/ece/cpen221/mp4/ai/FoxAI.java
@@ -28,9 +28,137 @@ public class FoxAI extends AbstractAI {
 
 	@Override
 	public Command getNextAction(ArenaWorld world, ArenaAnimal animal) {
-		// TODO: Change this. Implement your own AI to make decisions regarding
-		// the next action.
+		int x = animal.getLocation().getX();
+		int y = animal.getLocation().getY();
+		
+		int N = 0,S = 0,E = 0,W = 0; //Directional weights (positive is good)
+		int eatDesire = (int) (closest * (1.0 - ((double) animal.getEnergy() / animal.getMaxEnergy())));
+		for(Item i : world.searchSurroundings(animal)){
+			if(i.getStrength() > animal.getStrength()){
+				N = N - (closest * getYDist(animal, i));
+				S = S + (closest * getYDist(animal, i));
+				E = E - (closest * getXDist(animal, i));
+				W = W + (closest * getXDist(animal, i));
+			}
+			else if (i.getMeatCalories() > 0){
+				N = N - (eatDesire * getYDist(animal, i));
+				S = S + (eatDesire * getYDist(animal, i));
+				E = E - (eatDesire * getXDist(animal, i));
+				W = W + (eatDesire * getXDist(animal, i));
+			}
+		}
+		//Weights are assigned
+		
+		int[] weights = {N,S,E,W};
+		
+		sortWeights(weights);
+		
+		for(int i = 3; i >= 0; i--){
+			Item activeItem = null;
+			if(N == weights[i]){
+				for(Item j : world.searchSurroundings(animal)){
+					if(j.getLocation().getX() == x && j.getLocation().getY() == y+1){
+						activeItem = j;
+						break;
+					}
+				}
+				if(activeItem == null){
+					if(Util.isValidLocation(world,(new Location(x,y+1)))){
+						return new MoveCommand(animal,new Location(x,y+1));
+					}
+					else continue;
+				}
+					
+				else if(activeItem.getMeatCalories() > 0)
+					return new EatCommand(animal,activeItem);
+				else
+					continue;
+			}
+			
+			if(E == weights[i]){
+				for(Item j : world.searchSurroundings(animal)){
+					if(j.getLocation().getX() == x+1 && j.getLocation().getY() == y){
+						activeItem = j;
+						break;
+					}
+				}
+				if(activeItem == null){
+					if(Util.isValidLocation(world,(new Location(x+1,y)))){
+						return new MoveCommand(animal,new Location(x+1,y));
+					}
+					else continue;
+				}
+					
+				else if(activeItem.getMeatCalories() > 0)
+					return new EatCommand(animal,activeItem);
+				else
+					continue;
+			}
+			
+			if(S == weights[i]){
+				for(Item j : world.searchSurroundings(animal)){
+					if(j.getLocation().getX() == x && j.getLocation().getY() == y-1){
+						activeItem = j;
+						break;
+					}
+				}
+				if(activeItem == null){
+					if(Util.isValidLocation(world,(new Location(x,y-1)))){
+						return new MoveCommand(animal,new Location(x,y-1));
+					}
+					else continue;
+				}
+					
+				else if(activeItem.getMeatCalories() > 0)
+					return new EatCommand(animal,activeItem);
+				else
+					continue;
+			}
+			
+			else if(W == weights[i]){
+				for(Item j : world.searchSurroundings(animal)){
+					if(j.getLocation().getX() == x-1 && j.getLocation().getY() == y){
+						activeItem = j;
+						break;
+					}
+				}
+				if(activeItem == null){
+					if(Util.isValidLocation(world,(new Location(x-1,y)))){
+						return new MoveCommand(animal,new Location(x-1,y));
+					}
+					else continue;
+				}
+					
+				else if(activeItem.getMeatCalories() > 0)
+					return new EatCommand(animal,activeItem);
+				else
+					continue;
+			}
+			
+		}
+		
 		return new WaitCommand();
 	}
-
+	
+	int getXDist(Item a, Item b){
+		return b.getLocation().getX() - a.getLocation().getX();
+	}
+	
+	int getYDist(Item a, Item b){
+		return b.getLocation().getY() - a.getLocation().getY();
+	}
+	
+	//sorts array from lowest to highest
+	public void sortWeights(int[] w){
+		for(int i = 0; i < w.length; i++){
+			for(int j = 0; j < w.length - 1; j++){
+				if(w[j] > w[j+1]){
+					int temp = w[j];
+					w[j] = w[j+1];
+					w[j+1] = temp;
+				}
+			}
+				
+		}
+	}
 }
